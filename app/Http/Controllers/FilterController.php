049e06f291edef6852d426cd038fde1f461cdc6e
Justin Bauer <kapotchy@gmail.com>
2016-03-09 17:55:51 -0700

    Got some of the filtering sorted out..pagination is still not working tho

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\Controller;
 //use App\Http\Requests\PostFormRequest; // don't use for validation anymore
 use Illuminate\Http\Request;
 use GrahamCampbell\Markdown\Facades\Markdown; // use this to convert markdown to html
+use Illuminate\Pagination\Paginator;
 
 
 class FilterController extends Controller
@@ -27,38 +28,72 @@ class FilterController extends Controller
 
     }
 
-    public function vendor($vendor){
-        //$where = ['active' => 1, '']
+    public function vendor(Request $request)
+    {
+        $filterCompiler = $request->input('compiler');
+        if(is_numeric($filterCompiler)) {
+            $filterCompiler = McuCompilers::where('name', $filterCompiler)->first();
+            $compFilter = array('vendor_id' => 1, 'id' => $filterCompiler->id);
+        }
+        else
+            $compFilter = array('vendor_id' => 1);
 
         $posts = Posts::where('active', 1)
             ->with('mcu')
             ->with('tagged')
             ->with('categories')
             ->with('compiler')
-            ->withAnyTag(['elephant'])
-            ->whereHas('categories', function($q){
-                $q->where('slug', 'analog');
-            })
-            ->whereHas('languages', function($q){
-                $q->where('slug', 'c');
-            })
-            ->whereHas('compiler', function($q){
-                $q->where('id', 3);
+            // ->withAnyTag(['elephant'])
+//            ->whereHas('categories', function($q){
+//                $q->where('slug', 'analog');
+//            })
+//            ->whereHas('languages', function($q){
+//                $q->where('slug', 'c');
+//            })
+            ->whereHas('compiler', function ($q) use ($compFilter) {
+                //$q->where('id', 2);
+                $q->where($compFilter);
             })
             ->orderBy('created_at', 'desc')
-            ->paginate(5);
-            //->get();
+        //    ->paginate(5);
+        ->get();
+        ;
 
-            //Can use either get() or paginate() NOT BOTH
+        // Cycle through posts to get how many tags and such of each
+        foreach($posts as $post){
+           // $arr ['name'] = $post->compiler->name;
+           // $arr ['slug'] = $post->compiler->slug;
+            $compilers [] = $post->compiler->name;
+        }
+        $vals = array_count_values($compilers);
+        //print_r($vals); die;
 
-           // ->where('compiler.id', 3)
-           // ->where('mcu.vendor_id', 2);
+//        $categories = Categories::all();
+//        $mcus = Mcus::orderBy('vendor_id')->where('vendor_id',1)->get();
+//        $compilers = McuCompilers::orderBy('vendor_id')->where('vendor_id', 1)->get();
+//        $languages = McuLanguages::orderBy('id')->get();
+//        $tags = Posts::with('tagged')->first();
 
-       // $posts = Posts::with('categories')->get()->where('categories.slug', 'audio');
+        //Can use either get() or paginate() NOT BOTH
+
+        // ->where('compiler.id', 3)
+        // ->where('mcu.vendor_id', 2);
+
+        // $posts = Posts::with('categories')->get()->where('categories.slug', 'audio');
 
         //->orderBy('created_at', 'desc')->paginate(5)->get()
+        // print_r($mcus); die;
+        $posts = new Paginator($posts, 4, 1,array('path' => '/vendor/microchip')); // create pagination
+        $pagination = new Paginator(range(1,200), 10, 5);
 
-    print_r($posts); die;
-        return view('filter');
+        return view('filter')
+            ->withPosts($posts)
+            ->withPagination($pagination)
+//            ->withCategories($categories)
+//            ->withMcus($mcus)
+            ->withCompilers($vals)
+//            ->withLanguages($languages)
+//            ->withTags($tags)
+            ;
     }
 }
