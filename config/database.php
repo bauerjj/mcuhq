eab7786fce4b8c3816965074f2dab0074c76d0c1
Justin <kapotchy@gmail.com>
2016-03-22 00:21:32 -0700

    omitting db username/password. No longer in use....this uses the .env file

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'test'),
-            'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'baboosh'),
+            'database'  => env('DB_DATABASE', 'dbname'),
+            'username'  => env('DB_USERNAME', 'user'),
+            'password'  => env('DB_PASSWORD', 'password'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
